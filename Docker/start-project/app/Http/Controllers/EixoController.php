1407cd263c7ed9a002cc014d4ff7e0f81b7a379f
Francis Lovo <francis.lovo@mjv.com.br>
2024-03-06 11:35:27 -0300

    controller de turma

--- Docker/start-project/app/Http/Controllers/EixoController.php
+++ Docker/start-project/app/Http/Controllers/EixoController.php
@@ -52,10 +52,11 @@ class EixoController extends Controller {
     }
 
     public function destroy(string $id) {
-        if($this->repository->delete($id))  {
-            return "<h1>Delete - OK!</h1>";
-        }
+        return "ok";
+        //if($this->repository->delete($id))  {
+           // return "<h1>Delete - OK!</h1>";
+       // }
 
-        return "<h1>Delete - Not found Eixo!</h1>";
+       // return "<h1>Delete - Not found Eixo!</h1>";
     }
 }
